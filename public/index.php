048d222dc59ca24b843248dc06c39c290d3da6da
cgrant992 <caden.grant@gmail.com>
2023-06-25 14:14:52 -0700

    Added request and response

--- public/index.php
+++ public/index.php
@@ -1,5 +1,14 @@
 <?php declare(strict_types=1);
 
+use Caden\Framework\Http\Request;
+use Caden\Framework\Http\Response;
+
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
-dd('Hello');
+$request = Request::createFromGlobals();
+
+$content = '<h1>Hello World</h1>';
+
+$response = new Response(content: $content, status: 200, headers: []);
+
+$response->send();
